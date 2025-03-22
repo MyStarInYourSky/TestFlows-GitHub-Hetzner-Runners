51a4e45d4e31fa8f3c06a403f15720a40c4a338a
starlightcrafted <9060865+starlightcrafted@users.noreply.github.com>
2025-03-22 18:14:01 +0000

    Update setup.sh

--- testflows/github/hetzner/runners/scripts/setup.sh
+++ testflows/github/hetzner/runners/scripts/setup.sh
@@ -26,5 +26,6 @@ set -x
     apt-get install --yes --no-install-recommends \
         python3-venv
     su ubuntu -s python3 -m venv /home/ubuntu/venv
+    ls -la /home/ubuntu
     echo -n 'export PATH="/home/ubuntu/venv/bin/:$PATH"' >> /home/ubuntu/.profile
 }
